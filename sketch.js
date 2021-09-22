ecda3b35c7f2a8e481dc16d3b7b449c85ec87ba7
sdrrv <duartecelvas@gmail.com>
2021-09-22 21:20:04 +0100

    Zoom slider added

--- sketch.js
+++ sketch.js
@@ -1,12 +1,14 @@
 function setup() {
     createCanvas(windowWidth, windowHeight);
     //noCursor();
-    print(windowHeight);
+    slider = createSlider(30, 200, 50);
+    slider.position(windowWidth * 0.98 - slider.width, windowHeight * 0.94 - slider.height);
+    slider.style('height', '80px');
 }
 
 function draw() {
     background(220);
-    createGrid(50);
+    createGrid(slider.value());
 
 }
 
@@ -18,7 +20,7 @@ function createGrid(zoom) {
     for (var i = 10; i < width; i += zoom) {
 
         for (var j = 10; j < height; j += zoom) {
-            circle(i, j, 3);
+            circle(i, j, zoom * 0.06);
         }
 
     }
