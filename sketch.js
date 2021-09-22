049a966217e21033ce8bdd8d22cb8889d99eedb7
sdrrv <duartecelvas@gmail.com>
2021-09-22 22:34:35 +0100

    Mouse Over Effect

--- sketch.js
+++ sketch.js
@@ -1,14 +1,15 @@
 function setup() {
     createCanvas(windowWidth, windowHeight);
     //noCursor();
-    slider = createSlider(30, 200, 50);
-    slider.position(windowWidth * 0.98 - slider.width, windowHeight * 0.94 - slider.height);
-    slider.style('height', '80px');
+    zoomSlider = createSlider(30, 200, 50);
+    zoomSlider.position(windowWidth * 0.98 - zoomSlider.width, windowHeight * 0.94 - zoomSlider.height);
+    zoomSlider.style('height', '80px');
+    grid = [];
 }
 
 function draw() {
     background(220);
-    createGrid(slider.value());
+    createGrid(zoomSlider.value(), 8);
 
 }
 
@@ -16,12 +17,21 @@ function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
 
-function createGrid(zoom) {
+function createGrid(zoom, maxSize) {
     for (var i = 10; i < width; i += zoom) {
 
         for (var j = 10; j < height; j += zoom) {
-            circle(i, j, zoom * 0.06);
+            grid.push(circle(i, j, zoom * 0.06 + getSizeToGrow(i, j, maxSize)));
         }
 
     }
+}
+
+function getSizeToGrow(x, y, maxSize) {
+    var distance = dist(x, y, mouseX, mouseY);
+    var res = 200 / distance;
+    if (res > maxSize)
+        return maxSize;
+
+    return res;
 }
